49956e183b1191cb5276c0ae4194d84663a39e3c
sunm-in <sunm309@naver.com>
2021-08-26 01:19:12 +0900

    🩹Motion - image.ts (태그 수정)

--- motion/src/components/page/item/image.ts
+++ motion/src/components/page/item/image.ts
@@ -5,7 +5,7 @@ export class ImageComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
     super(`<section class="image">
             <div class="image__holder"><img src="" alt="" class="image__thumbnail"></div>
-            <p class="image__title"></p>
+            <h2 class="image__title"></h2>
           </section>`);
 
     // 서용자에게 전달받은 데이터를 innerHTML에 설정하는 것을 좋지 않고 이렇게 필요한 부분만 업데이트해 주는 것이 더 안전하다.
