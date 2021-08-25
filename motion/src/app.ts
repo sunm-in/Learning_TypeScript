7766dafab5f23739d47243acf082bd291b9a7184
sunm-in <sunm309@naver.com>
2021-08-26 01:19:45 +0900

    🔨 Motion - app.ts (video, note, todo 추가)

--- motion/src/app.ts
+++ motion/src/app.ts
@@ -1,6 +1,9 @@
 // components
-import { ImageComponent } from './components/page/item/image.js';
 import { PageComponent } from './components/page/page.js';
+import { ImageComponent } from './components/page/item/image.js';
+import { TodoComponent } from './components/page/item/todo.js';
+import { NoteComponent } from './components/page/item/note.js';
+import { VideoComponent } from './components/page/item/video.js';
 
 // 전달받은 최고의 컨테이너 안에 페이지라는 컴포넌트를 만들어서, 만들어진 페이지를 붙여주는 일
 class App {
@@ -14,6 +17,18 @@ class App {
       'https://picsum.photos/600/300'
     );
     image.attachTo(appRoot, 'beforeend');
+
+    const video = new VideoComponent(
+      'Video Title',
+      'https://youtu.be/BdkSkI61aGo'
+    );
+    video.attachTo(appRoot, 'beforeend');
+
+    const note = new NoteComponent('Note Title', 'Note Body');
+    note.attachTo(appRoot, 'beforeend');
+
+    const todo = new TodoComponent('Todo Title', 'Todo Item');
+    todo.attachTo(appRoot, 'beforeend');
   }
 }
 
