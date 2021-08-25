07dae9e68ad4b72be2ade322a94ead76c2d6d337
sunm-in <sunm309@naver.com>
2021-08-26 01:18:38 +0900

    🔧 Motion - video 뼈대 작업

---
+++ motion/src/components/page/item/video.ts
@@ -0,0 +1,49 @@
+import { BaseComponent } from './../../component.js';
+export class VideoComponent extends BaseComponent<HTMLElement> {
+  constructor(title: string, url: string) {
+    super(`<section class="video">
+            <div class="video__player"><iframe class="video__iframe"></iframe></div>
+            <h3 class="video__title"></h3>
+          </section>`);
+
+    const iframe = this.element.querySelector(
+      '.video__iframe'
+    )! as HTMLIFrameElement;
+
+    // url -> videoId -> embed
+    iframe.src = this.convertToEmbeddedURL(url);
+
+    const titleElement = this.element.querySelector(
+      '.video__title'
+    )! as HTMLHeadingElement;
+    titleElement.textContent = title;
+  }
+
+  // input
+  // https://www.youtube.com/watch?v=BdkSkI61aGo
+  // https://youtu.be/BdkSkI61aGo
+
+  // output
+  // https://www.youtube.com/embed/BdkSkI61aGo
+  // 정규표현식 Regex -> 문자열의 다수의 특정한 패턴에서 해당하는 문자열, 해당하는 패턴을 가지고 올 수 있는 것
+  private convertToEmbeddedURL(url: string): string {
+    const regExp =
+      /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+    const match = url.match(regExp);
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    return url;
+  }
+}
+
+// <iframe
+//   width="950"
+//   height="534"
+//   src="https://www.youtube.com/embed/BdkSkI61aGo"
+//   title="YouTube video player"
+//   frameborder="0"
+//   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//   allowfullscreen
+// ></iframe>;
