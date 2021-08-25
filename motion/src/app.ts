745d06e366662bbc0ac49f732e42bb962b24d34e
sunm-in <sunm309@naver.com>
2021-08-25 11:54:56 +0900

    🎨 중복된 코드 component.ts에 정의

--- motion/src/app.ts
+++ motion/src/app.ts
@@ -1,11 +1,21 @@
-import { PageComponent } from './components/page.js';
+// components
+import { ImageComponent } from './components/page/item/image.js';
+import { PageComponent } from './components/page/page.js';
 
+// 전달받은 최고의 컨테이너 안에 페이지라는 컴포넌트를 만들어서, 만들어진 페이지를 붙여주는 일
 class App {
   private readonly page: PageComponent;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
+
+    const image = new ImageComponent(
+      'Image Title',
+      'https://picsum.photos/600/300'
+    );
+    image.attachTo(appRoot, 'beforeend');
   }
 }
 
+// Type Assertion
 new App(document.querySelector('.document')! as HTMLElement);
---
+++ motion/src/components/component.ts
@@ -0,0 +1,24 @@
+export interface Component {
+  attachTo(parent: HTMLElement, position?: InsertPosition): void;
+}
+
+/**
+ * Encapsulate the HTML element creation
+ */
+
+// 제네릭 T타입 -> 반드시 HTMLElement이거나 HTMLElement를 상속하는 서브클래스에만 가능
+export class BaseComponent<T extends HTMLElement> implements Component {
+  // 외부에서 볼 수 없고 이것을 상속하는 자식 클래스에서만 접근 가능
+  // 한번 만들어진 element는 읽기만 가능, 요소를 만들면 요소 안에 상태들은 변경이 가능하지만 요소 자체를 다른 것으로 변경하는 것은 불가
+  protected readonly element: T;
+  constructor(htmlString: string) {
+    const template = document.createElement('template');
+    template.innerHTML = htmlString;
+    this.element = template.content.firstElementChild! as T;
+  }
+
+  // parent: HTMLElement라면, 즉 HTMLElement를 상속하는 그 어떤 자식 요소들도 전달할 수 있다.
+  attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
+    parent.insertAdjacentElement(position, this.element);
+  }
+}
