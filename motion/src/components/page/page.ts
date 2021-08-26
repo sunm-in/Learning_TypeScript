84eeb7b1fb2562306255ec9153e6c810aff16027
sunm-in <sunm309@naver.com>
2021-08-26 23:44:35 +0900

    🔨 PageItemComponent 생성 - close 버튼 추가

--- motion/src/components/page/page.ts
+++ motion/src/components/page/page.ts
@@ -1,10 +1,65 @@
 // component
-import { BaseComponent } from './../component.js';
+import { BaseComponent, Component } from './../component.js';
+
+export interface Composable {
+  addChild(child: Component): void;
+}
+type OnCloseListener = () => void;
+
+interface SectionContainer extends Component, Composable {
+  setOnCloseListener(listener: OnCloseListener): void;
+}
+
+type SectionContainerConstructor = {
+  new (): SectionContainer;
+};
+
+export class PageItemComponent
+  extends BaseComponent<HTMLElement>
+  implements SectionContainer
+{
+  private closeListener?: OnCloseListener;
+
+  constructor() {
+    super(`<li class="page-item">
+            <section class="page-item__body">
+              <div class="page-item__controls">
+                <button class="close">&times;</button>
+              </div>
+            </section>
+          </li>`);
+    const closeBtn = this.element.querySelector('.close')! as HTMLButtonElement;
+    closeBtn.onclick = () => {
+      this.closeListener && this.closeListener();
+    };
+  }
+  addChild(child: Component) {
+    const container = this.element.querySelector(
+      '.page-item__body'
+    )! as HTMLElement;
+    child.attachTo(container);
+  }
+  setOnCloseListener(listener: OnCloseListener) {
+    this.closeListener = listener;
+  }
+}
 
 // element라는 내부 스테이트, 이 element는 DOM 요소 중의 하나로 생성자 안에서 우리가 원하는 DOM 요소를 만들어 준 다음에
 // attachTo라는 함수를 호출해야지만 전달받은 parent 요소에 우리가 만든 DOM 요소를 붙여주는 일을 하고 있다.
-export class PageComponent extends BaseComponent<HTMLUListElement> {
-  constructor() {
-    super('<ul class="page">This is PageComponent!</ul>');
+export class PageComponent
+  extends BaseComponent<HTMLUListElement>
+  implements Composable
+{
+  constructor(private pageItemConstructor: SectionContainerConstructor) {
+    super('<ul class="page"></ul>');
+  }
+
+  addChild(section: Component) {
+    const item = new this.pageItemConstructor();
+    item.addChild(section);
+    item.attachTo(this.element, 'beforeend');
+    item.setOnCloseListener(() => {
+      item.removeFrom(this.element);
+    });
   }
 }
