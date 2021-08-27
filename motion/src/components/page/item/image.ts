1d0bf184b32f2bf3d6027e68e2f24f815f64d93f
sunm-in <sunm309@naver.com>
2021-08-27 13:24:01 +0900

    💄 스타일링 작업 완료

--- motion/src/components/page/item/image.ts
+++ motion/src/components/page/item/image.ts
@@ -5,18 +5,18 @@ export class ImageComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
     super(`<section class="image">
             <div class="image__holder"><img src="" alt="" class="image__thumbnail"></div>
-            <h2 class="image__title"></h2>
+            <h2 class="page-item__title image__title"></h2>
           </section>`);
 
     // 서용자에게 전달받은 데이터를 innerHTML에 설정하는 것을 좋지 않고 이렇게 필요한 부분만 업데이트해 주는 것이 더 안전하다.
     const imageElement = this.element.querySelector(
-      '.image__thumbnail'
+      '.image__thumbnail',
     )! as HTMLImageElement;
     imageElement.src = url;
     imageElement.alt = title;
 
     const titleElement = this.element.querySelector(
-      '.image__title'
+      '.image__title',
     )! as HTMLParagraphElement;
     titleElement.textContent = title;
   }
--- motion/src/components/page/item/video.ts
+++ motion/src/components/page/item/video.ts
@@ -3,18 +3,18 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
     super(`<section class="video">
             <div class="video__player"><iframe class="video__iframe"></iframe></div>
-            <h3 class="video__title"></h3>
+            <h3 class="page-item__title video__title"></h3>
           </section>`);
 
     const iframe = this.element.querySelector(
-      '.video__iframe'
+      '.video__iframe',
     )! as HTMLIFrameElement;
 
     // url -> videoId -> embed
     iframe.src = this.convertToEmbeddedURL(url);
 
     const titleElement = this.element.querySelector(
-      '.video__title'
+      '.video__title',
     )! as HTMLHeadingElement;
     titleElement.textContent = title;
   }
