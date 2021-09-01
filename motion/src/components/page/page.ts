63e328cbced571cb2971a21ec0e758eafc14daa1
sunm-in <sunm309@naver.com>
2021-09-01 23:41:42 +0900

    🔨 decorator 적용 및 프로젝트 마무리

--- motion/src/components/page/page.ts
+++ motion/src/components/page/page.ts
@@ -1,4 +1,10 @@
 // component
+import {
+  EnableDragging,
+  EnableDrop,
+  EnableHover,
+} from '../../decorators/draggable.js';
+import { Draggable, Droppable, Hoverable } from '../common/type.js';
 import { BaseComponent, Component } from './../component.js';
 
 export interface Composable {
@@ -11,16 +17,20 @@ type OnDragStateListener<T extends Component> = (
   state: DragState,
 ) => void;
 
-interface SectionContainer extends Component, Composable {
+interface SectionContainer extends Component, Composable, Draggable, Hoverable {
   setOnCloseListener(listener: OnCloseListener): void;
   setOnDragStateListener(listener: OnDragStateListener<SectionContainer>): void;
   muteChildren(state: 'mute' | 'unmute'): void;
+  getBoundingRect(): DOMRect;
+  onDropped(): void;
 }
 
 type SectionContainerConstructor = {
   new (): SectionContainer;
 };
 
+@EnableDragging
+@EnableHover
 export class PageItemComponent
   extends BaseComponent<HTMLElement>
   implements SectionContainer
@@ -40,31 +50,26 @@ export class PageItemComponent
     closeBtn.onclick = () => {
       this.closeListener && this.closeListener();
     };
-
-    this.element.addEventListener('dragstart', (event: DragEvent) => {
-      this.onDragStart(event);
-    });
-    this.element.addEventListener('dragend', (event: DragEvent) => {
-      this.onDragEnd(event);
-    });
-    this.element.addEventListener('dragenter', (event: DragEvent) => {
-      this.onDragEnter(event);
-    });
-    this.element.addEventListener('dragleave', (event: DragEvent) => {
-      this.onDragLeave(event);
-    });
   }
   onDragStart(_: DragEvent) {
     this.notifyDragObservers('start');
+    this.element.classList.add('lifted');
   }
   onDragEnd(_: DragEvent) {
     this.notifyDragObservers('stop');
+    this.element.classList.remove('lifted');
   }
   onDragEnter(_: DragEvent) {
     this.notifyDragObservers('enter');
+    this.element.classList.add('drop-area');
   }
   onDragLeave(_: DragEvent) {
     this.notifyDragObservers('leave');
+    this.element.classList.remove('drop-area');
+  }
+
+  onDropped() {
+    this.element.classList.remove('drop-area');
   }
 
   notifyDragObservers(state: DragState) {
@@ -93,13 +98,18 @@ export class PageItemComponent
       this.element.classList.remove('mute-children');
     }
   }
+
+  getBoundingRect(): DOMRect {
+    return this.element.getBoundingClientRect();
+  }
 }
 
 // element라는 내부 스테이트, 이 element는 DOM 요소 중의 하나로 생성자 안에서 우리가 원하는 DOM 요소를 만들어 준 다음에
 // attachTo라는 함수를 호출해야지만 전달받은 parent 요소에 우리가 만든 DOM 요소를 붙여주는 일을 하고 있다.
+@EnableDrop
 export class PageComponent
   extends BaseComponent<HTMLUListElement>
-  implements Composable
+  implements Composable, Droppable
 {
   private children = new Set<SectionContainer>();
   private dropTarget?: SectionContainer;
@@ -107,30 +117,22 @@ export class PageComponent
 
   constructor(private pageItemConstructor: SectionContainerConstructor) {
     super('<ul class="page"></ul>');
-    this.element.addEventListener('dragover', (event: DragEvent) => {
-      this.onDragOver(event);
-    });
-    this.element.addEventListener('drop', (event: DragEvent) => {
-      this.onDrop(event);
-    });
   }
-
-  onDragOver(event: DragEvent) {
-    event.preventDefault();
-    console.log('onDragOver');
-    // 여기에서 위치를 바꿔주기
+  onDragOver(_: DragEvent): void {}
+  onDrop(event: DragEvent) {
     if (!this.dropTarget) {
       return;
     }
     if (this.dragTarget && this.dragTarget !== this.dropTarget) {
+      const dropY = event.clientY;
+      const srcElement = this.dragTarget.getBoundingRect();
       this.dragTarget.removeFrom(this.element);
-      this.dropTarget.attach(this.dragTarget, 'beforebegin');
+      this.dropTarget.attach(
+        this.dragTarget,
+        dropY < srcElement.y ? 'beforebegin' : 'afterend',
+      );
+      this.dragTarget.onDropped();
     }
-  }
-
-  onDrop(event: DragEvent) {
-    event.preventDefault();
-    console.log('onDrop');
   }
 
   addChild(section: Component) {
